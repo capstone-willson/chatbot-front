2e1f7a1230412c9a07d14d7d550e8e5be870ffc5
taeuk.kang <gmdkrl@hanyang.ac.kr>
2019-05-06 07:23:09 +0900

    관리자 페이지 - 데이터 수집 콘텐츠 구현

--- src/components/bot-setting-window-body.js
+++ src/components/bot-setting-window-body.js
@@ -1,4 +1,5 @@
 import {html, render} from '../../node_modules/lit-html/lit-html.js'
+import './modal-food.js'
 
 class BotSettingBody extends HTMLElement {
 	constructor() {
@@ -7,14 +8,20 @@ class BotSettingBody extends HTMLElement {
 		render(this.render(), this.shadowRoot)		
 		
 		this.eventClickProcessBox = this.onClickProcessBox.bind(this)
+		this.eventClickSubBox = this.onClickSubBox.bind(this)
+		this.eventAnimatedEnd = this.onAnimatedEnd.bind(this)
 	}
 
 	connectedCallback() {				
 		this.shadowRoot.querySelector(`.process-data-collection`).addEventListener(`click`, this.eventClickProcessBox, true)
+		this.shadowRoot.querySelector(`.process-data.food`).addEventListener(`click`, this.eventClickSubBox, true)
+		this.shadowRoot.querySelector(`main`).addEventListener(`transitionend`, this.eventAnimatedEnd, true)
 	}
 
 	disconnectedCallback() {
-		this.shadowRoot.querySelector(`.process-data-collection`).removeEventListener(`click`, this.eventClickProcessBox, true)		
+		this.shadowRoot.querySelector(`.process-data-collection`).removeEventListener(`click`, this.eventClickProcessBox, true)
+		this.shadowRoot.querySelector(`.process-data.food`).removeEventListener(`click`, this.eventClickSubBox, true)	
+		this.shadowRoot.querySelector(`main`).removeEventListener(`transitionend`, this.eventAnimatedEnd, true)
 	}	
 
 	onClickProcessBox(event) {
@@ -25,8 +32,45 @@ class BotSettingBody extends HTMLElement {
 
 			array.forEach(each => {
 				this.shadowRoot.querySelector(each).classList.toggle(`clicked`)
-			})			
+			})
 		}
+	}
+
+	onClickSubBox(event) {
+		const target = event.target.closest(`.process-data`)
+
+		if (target) {
+			this.shadowRoot.querySelector(`main`).insertAdjacentHTML(`beforeend`, `<modal-food></modal-food>`)
+		}		
+	}
+
+	onAnimatedEnd(event) {
+		const target = event.target
+		if (event.propertyName === `margin-left` && target.classList.contains(`process-data-collection`)) {
+			this.emptyLine()
+			this.connectLine(target, this.shadowRoot.querySelector(`.food`))
+			this.connectLine(target, this.shadowRoot.querySelector(`.bus`))
+			this.connectLine(target, this.shadowRoot.querySelector(`.queries`))
+			this.connectLine(target, this.shadowRoot.querySelector(`.questions`))
+		}
+	}
+
+	emptyLine() {
+		const svg = this.shadowRoot.querySelector(`.svg-line`)
+		svg.innerHTML = ``
+	}
+
+	connectLine(div1, div2) {	
+		const svg = this.shadowRoot.querySelector(`.svg-line`)
+
+		svg.innerHTML +=`
+				<line
+					class = 'line'
+					x1 = '${div1.offsetLeft + div1.offsetWidth/2}'
+					y1 = '${div1.offsetTop + div1.offsetHeight/2 + 20}'
+					x2 = '${div2.offsetLeft + div2.offsetWidth/2 - 30}'
+					y2 = '${div2.offsetTop + div2.offsetHeight/2 - 20}'></line>
+			`
 	}
 
 	render() {
@@ -38,23 +82,25 @@ class BotSettingBody extends HTMLElement {
 					<p>${i18next.t(`PROCESS_DATA_COLLECTION`)}</p>					
 				</span>
 
-				<span class='process-sub-box food'>
+				<!-- 데이터 수집 하위박스 START -->
+				<span class='process-sub-box process-data food'>
 					<object class='process-box-img' type="image/svg+xml" data="../../images/food.svg" width='30' height='30'></object>
 					<p>${i18next.t(`PROCESS_DATA_FOOD`)}</p>			
 				</span>
-				<span class='process-sub-box bus'>
+				<span class='process-sub-box process-data bus'>
 					<object class='process-box-img' type="image/svg+xml" data="../../images/bus.svg" width='30' height='30'></object>
 					<p>${i18next.t(`PROCESS_DATA_BUS`)}</p>	
 				</span>
-				<span class='process-sub-box queries'>
+				<span class='process-sub-box process-data queries'>
 					<object class='process-box-img' type="image/svg+xml" data="../../images/queries.svg" width='30' height='30'></object>
 					<p>${i18next.t(`PROCESS_DATA_QUERY`)}</p>	
 				</span>
-				<span class='process-sub-box questions'>
+				<span class='process-sub-box process-data questions'>
 					<object class='process-box-img' type="image/svg+xml" data="../../images/question.svg" width='30' height='30'></object>
 					<p>${i18next.t(`PROCESS_DATA_QUESTION`)}</p>	
 				</span>
-				
+				<!-- 데이터 수집 하위박스 END -->
+
 				<span class='process-box process-data-analysis'>
 					<object class='process-box-img' type="image/svg+xml" data="../../images/search.svg" width='50' height='50'></object>
 					<p>${i18next.t(`PROCESS_DATA_ANALYSIS`)}</p>
@@ -68,6 +114,8 @@ class BotSettingBody extends HTMLElement {
 					<p>${i18next.t(`PROCESS_DATA_VISUALIZATION`)}</p>
 				</span>
 			</main>
+
+			<svg class='svg-line'></svg>			
 		`
 	}
 }
@@ -135,19 +183,19 @@ const style = html`
 	}
 
 	.process-data-collection {
-		background-color: #379392;
+		background-color: #6B7EFC;
 	}
 
 	.process-data-analysis {
-		background-color: #4FB0C6;
+		background-color: #6B7EFC;
 	}
 
 	.process-data-detail-analysis {
-		background-color: #4F86C6;
+		background-color: #6B7EFC;
 	}
 
 	.process-data-visualization {
-		background-color: #6C49B8;
+		background-color: #6B7EFC;
 	}
 
 	.process-box.clicked {
@@ -217,7 +265,7 @@ const style = html`
 		transition: all 0.1s ease-in;
 		font-size: 12px;
 		cursor: pointer;
-		z-index: 0;
+		z-index: 5;
 	}
 
 	.process-sub-box:hover {
@@ -226,31 +274,31 @@ const style = html`
 
 	@keyframes sub-bubble {
 		0% {
-			transform: scale(1) translate(-50%, -50%);
+			transform: translate(-50%, -50%) scale(1);
 		}
 
 		50% {
-			transform: scale(1.1) translate(-50%, -50%);
+			transform: translate(-50%, -50%) scale(1.1);
 		}
 
 		100% {
-			transform: scale(1) translate(-50%, -50%);
+			transform: translate(-50%, -50%) scale(1);
 		}
 	}
 
 	.food, .bus, .queries, .questions {
 		grid-area: a;
-		background-color: hsl(179, 46%, 60%);
+		background-color: #6B7EFC;
 	}
 
 	.process-sub-box.clicked {
 		display: flex;
 		transform: translate(-50%, -50%);
 		flex-flow: column;
-		z-index: 0;
+		z-index: 5;
 	}
 
-	.food.clicked {			
+	.food.clicked {
 		top: calc(50% - 100px);
 		left: calc(50% + 100px);
 	}
@@ -269,6 +317,23 @@ const style = html`
 		top: calc(50% + 100px);
 		left: calc(50% - 100px);
 	}
+
+	.svg-line {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100vh;
+		pointer-events: none;
+		z-index: 0;
+	}
+
+	.line {		
+		stroke-width: 2px;
+		stroke: #6B7EFC;
+		stroke-dasharray: 4px;
+	}
+
 </style>
 `
 
---
+++ src/components/modal-food.js
@@ -0,0 +1,123 @@
+import {html, render} from '../../node_modules/lit-html/lit-html.js'
+
+class ModalFood extends HTMLElement {
+	constructor() {
+		super()
+
+		this.attachShadow({ mode: `open` })
+		render(this.render(), this.shadowRoot)
+
+		this.eventClickBack = this.onClickBack.bind(this)
+	}
+
+	connectedCallback() {
+		this.addEventListener(`click`, this.eventClickBack, false)
+		this.shadowRoot.addEventListener(`click`, event => event.stopPropagation(), false)
+
+		this.shadowRoot.querySelector(`iframe`).addEventListener(`load`, async () => {
+			const iframe = this.shadowRoot.querySelector(`iframe`).contentDocument
+			iframe.head.insertAdjacentHTML(`beforeend`, `
+				<style>
+					* {
+						font-size: 12px !important;
+					}
+
+					*::-webkit-scrollbar {
+						width: 0;
+					}
+
+					.curl {
+						min-width: 100% !important;
+					}
+
+					.topbar, .information-container, .scheme-container, .table-container, .response-col_status, .responses-table:not(.live-responses-table) {
+						display: none !important;
+					}
+				</style>
+			`)
+
+			iframe.querySelector(`#operations-User-get_hanyangfood_ > div`).click()
+			await this.sleep(500)
+			iframe.querySelector(`#operations-User-get_hanyangfood_ .try-out__btn`).click()
+			await this.sleep(500)
+			iframe.querySelector(`#operations-User-get_hanyangfood_ .execute`).click()			
+
+		})
+
+	}
+
+	disconnectedCallback() {
+		this.removeEventListener(`click`, this.eventClickBack, false)
+	}
+
+	onClickBack() {
+		this.style.display = `none`
+	}
+
+	sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms))
+	}
+
+	render() {
+		return html`
+			${style}
+			<div id='modalFood'>
+				<iframe src='http://localhost:8088/api-docs/'></iframe>
+			</div>
+		`
+	}	
+}
+
+const style = html`
+<style>
+	*::-webkit-scrollbar {
+		width: 0;
+		display: none;
+	}
+
+	:host {
+		display: flex;
+		justify-content: center;
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+		z-index: 50;
+	}
+
+	#modalFood {
+		position: absolute;
+		width: 400px;
+		height: 90vh;
+		align-self: center;
+		justify-self: center;
+		border-radius: 3px;
+		background-color: white;
+		overflow: scroll;
+		animation: up 0.1s ease-in-out;
+	}
+
+	@keyframes up {
+		0% {
+			width: 0;
+			height: 0;
+		}
+
+		100% {
+			width: 400px;
+			height: 90vh;
+		}
+	}
+
+	iframe {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+</style>
+`
+
+customElements.define(`modal-food`, ModalFood)
