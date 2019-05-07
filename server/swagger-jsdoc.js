bc85880f6ae38b69210cbd8e087f141afdba7a69
root <root@willson-server.asia-east1-b.c.cool-benefit-185923.internal>
2019-05-07 15:18:01 +0000

    로컬 > 서버 이전으로 셋팅 변경

--- server/swagger-jsdoc.js
+++ server/swagger-jsdoc.js
@@ -5,11 +5,11 @@ module.exports = {
 		// 정보
 		info: {
 			title: `Node.js Server API`,
-			version: `0.0.1`,
+			version: `0.0.2`,
 			description: `Node.js 서버 API 설명서`,
 		},
 		// 주소
-		host: `localhost:8080`,
+		host: `34.80.42.161:8080`,
 		// 기본 root path
 		basePath: `/`,
 		contact: {
